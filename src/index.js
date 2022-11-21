0ff8cb3a7775356f46c2273da1700d51113856e1
knagara <nagara.keita@gmail.com>
2022-11-21 13:30:44 +0900

    react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Index from "./routes/Index";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/Contact";
+import EditContact, { action as editAction } from "./routes/Edit";
+import { action as destroyAction } from "./routes/Destroy";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "app/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
